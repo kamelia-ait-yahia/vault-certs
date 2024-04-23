b7acfab9cbfdaf75eb6418e1ffceb110734a9a46
kamelia.ait-yahia.auditeur <kamelia.ait-yahia.auditeur@lecnam.net>
2024-04-23 14:35:02 +0200

    correct server cert generation

--- generate_certificate.sh
+++ generate_certificate.sh
@@ -11,8 +11,8 @@ export VAULT_TOKEN=root
 
 export VAULT_ROLE_NAME="app-role"
 export VAULT_CERT_NAME="consumer.app.com"
-export VAULT_CERT_SAN1_NAME="consumer2.app.com"
-export VAULT_CERT_SAN2_NAME="consumer3.app.com"
+export VAULT_CERT_SAN1_NAME="consumer.app.com"
+export VAULT_CERT_SAN2_NAME="ordinary.consumer.app.com"
 
 
 #set roleid and secretid as env variables from the previous step
@@ -21,11 +21,10 @@ export VAULT_PASSWORD="Aityahia"
 
 vault login -format=json -method=userpass \
     username=${VAULT_USER} \
-    password=${VAULT_PASSWORD} | jq -r .auth.client_token > user.token
+    password=${VAULT_PASSWORD} > certs/user.token
 
 #store the token as env variable, now this token can be used to authenticate against Vault
-export VAULT_TOKEN=`cat user.token`
-
+export VAULT_TOKEN=`token-value-from-user.token`
 
 echo -e "\n------------------    Read Role   --------------------"
 vault read pki_int/roles/$VAULT_ROLE_NAME
@@ -35,9 +34,9 @@ echo -e "\n------------------    Request Certificates   --------------------"
 vault write -format=json pki_int/issue/$VAULT_ROLE_NAME \
     common_name="$VAULT_CERT_NAME" \
     alt_names="$VAULT_CERT_SAN1_NAME,$VAULT_CERT_SAN2_NAME" \
-    ttl="3h" > certs/server/$VAULT_CERT_NAME.json
+    ttl="8000h" > certs/server/$VAULT_CERT_NAME.json
 '
 echo -e "\n------------------    extract the certificate, issuing ca in the pem file and private key in the key file seperately   --------------------"
-cat certs/server/$VAULT_CERT_NAME.json | jq -r .data.certificate > certs/server/$VAULT_CERT_NAME.crt
-cat certs/server/$VAULT_CERT_NAME.json | jq -r .data.issuing_ca >> certs/server/$VAULT_CERT_NAME.crt
-cat certs/server/$VAULT_CERT_NAME.json | jq -r .data.private_key > certs/server/$VAULT_CERT_NAME.key
+cat certs/server/consumer.app.com.json | jq -r .data.certificate > certs/server/consumer.app.com.crt
+cat certs/server/consumer.app.com.json | jq -r .data.issuing_ca >> certs/server/consumer.app.com.crt
+cat certs/server/consumer.app.com.json | jq -r .data.private_key > certs/server/consumer.app.com.key
